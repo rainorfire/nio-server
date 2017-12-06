6ac1bc6534287bdb5d363f9544af2b82bc92a6ca
cyj_m <cyj_m@192.168.0.107>
2017-12-06 23:55:40 +0800

    处理粘包问题

--- src/main/java/com/benny/spat/nio/server/net/NioServer.java
+++ src/main/java/com/benny/spat/nio/server/net/NioServer.java
@@ -3,11 +3,11 @@ package com.benny.spat.nio.server.net;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -15,17 +15,15 @@ import com.benny.spat.nio.server.IServer;
 
 public class NioServer implements IServer{
 	
-	private Selector selector;
-
 	public void start(int port) {
 		try {
-			this.selector = Selector.open();
+			Selector selector = Selector.open();
 			ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 			serverSocketChannel.bind(new InetSocketAddress(port));
 			serverSocketChannel.configureBlocking(false);
 			serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 			
-			new Thread(new NIOServer()).start();
+			new Thread(new NIOServer(selector)).start();
 			System.out.println("NIO服务器已启动...");
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -33,6 +31,12 @@ public class NioServer implements IServer{
 	}
 	
 	public class NIOServer implements Runnable{
+		
+		private Selector selector;
+		
+		public NIOServer(Selector selector) {
+			this.selector = selector;
+		}
 
 		public void run() {
 			while(true) {
@@ -59,14 +63,31 @@ public class NioServer implements IServer{
 							accept.configureBlocking(false);  
 							accept.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);  
 						}else if(selectionKey.isReadable()) {
-							ByteBuffer byteBuffer = ByteBuffer.allocate(512);
 							SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
 							try {
-								while(socketChannel.read(byteBuffer) > 0) {
-									byteBuffer.flip();
-									byte[] array = byteBuffer.array();
-									byteBuffer.clear();
-									System.out.println(new String(array));
+								int byteLength = 0;
+								int tmpLength = 0;
+								ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+								while((tmpLength = socketChannel.read(lengthBuffer)) > 0) {
+									if(tmpLength == 4) {
+										lengthBuffer.flip();
+										byteLength = lengthBuffer.getInt();
+										lengthBuffer.clear();
+										break;
+									}
+								}
+								if(byteLength > 0) {
+									tmpLength = 0;
+									ByteBuffer byteBuffer = ByteBuffer.allocate(byteLength);
+									while((tmpLength = socketChannel.read(byteBuffer)) <= byteLength) {
+										if(tmpLength == byteLength) {
+											byteBuffer.flip();
+											byte[] array = byteBuffer.array();
+											byteBuffer.clear();
+											System.out.println(new String(array));
+											break;
+										}
+									}
 								}
 								selectionKey.interestOps(SelectionKey.OP_READ);
 							}catch(Exception e) {
